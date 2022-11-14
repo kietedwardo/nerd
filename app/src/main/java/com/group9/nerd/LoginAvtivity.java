7efd86c20fc09764844e4475ead62a5f39cd1db5
kietedwardo <ryuzin790@gmail.com>
2022-11-15 02:29:33 +0700

    Login and Resgister

---
+++ app/src/main/java/com/group9/nerd/LoginAvtivity.java
@@ -0,0 +1,14 @@
+package com.group9.nerd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginAvtivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
